cd7c42a91a4f0e945cb3c5fa8ee71a608349744c
davidschedler <david.schedler.90@gmail.com>
2022-04-08 16:29:23 +0200

    added config module to get env variables, added jwt token to auth controller

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,17 @@
 import { PrismaClient } from '.prisma/client';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
-        super({
-            datasources: {
-                db: {
-                    url: 'postgresql://postgres:postgres@localhost:5434/cloudia-db?schema=public'
-                }
-            }
-        })
-    }
+  constructor(configService: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: configService.get('DATABASE_URL'),
+        },
+      },
+    });
+    //console.log(configService.get('DATABASE_URL'))
+  }
 }
