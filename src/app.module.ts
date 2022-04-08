5466755aa94431b996e16d4f3de3efa91203f052
davidschedler <david.schedler.90@gmail.com>
2022-04-08 17:02:23 +0200

    added jwt strategy and empty users controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
+import { UserModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 
