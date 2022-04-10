96c6e2aef770adfabd789c862cf00079a133209b
davidschedler <david.schedler.90@gmail.com>
2022-04-10 13:13:17 +0200

    improvments to aws controller

--- src/provider/aws/aws.service.ts
+++ src/provider/aws/aws.service.ts
@@ -1,19 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { PrismaService } from '../../prisma/prisma.service';
+import { AwsSimpleDto } from './dto';
 
 @Injectable()
 export class AwsService {
-    constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
-    async getAwsSimpleDataAll() {
-        return 'TODO'
+  async getAwsSimpleDataAll() {
+    try {
+      const awsSimple: AwsSimpleDto[] = await this.prisma.awsSimple.findMany(
+        {},
+      );
+      return awsSimple;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code == 'P2008') {
+          throw new NotFoundException('No AwsSimple data found!');
+        }
+      }
+      throw error;
     }
+  }
 
-    async getAwsSimpleDataProduct() {
-        return 'TODO'
+  async getAwsSimpleDataProduct(product: string) {
+    try {
+      const awsSimple: AwsSimpleDto = await this.prisma.awsSimple.findUnique({
+        where: {
+          InstanceType: product,
+        },
+      });
+      return awsSimple;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code == 'P2008') {
+          throw new NotFoundException('No AwsSimple data found!');
+        }
+      }
+      throw error;
     }
+  }
 
-    async getAwsSimpleDataOptions() {
-        return 'TODO'
+  async getAwsSimpleDataOptions() {
+    try {
+      const awsSimple = await this.prisma.awsSimple.findMany({});
+      
+      //TODO EXTRACT ALL MEMORY / VCPUS etc TYPES and return it
+      
+      
+      return 'TODO';
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code == 'P2008') {
+          throw new NotFoundException('No AwsSimple data found!');
+        }
+      }
+      throw error;
     }
+  }
 }
---
+++ src/provider/aws/dto/awssimpleoptions.dto.ts
@@ -0,0 +1,4 @@
+export class AwsSimpleOptionsDto {
+
+    memoryMap
+}
